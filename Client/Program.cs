6f135d557f27ae4f719d1dbfb55da5ad03561cde
Tarik Guney <atarikguney@gmail.com>
2018-12-18 08:56:57 -0700

    Implementing file watcher with glob

--- Client/Program.cs
+++ Client/Program.cs
@@ -23,8 +23,24 @@ namespace Copier.Client
         private static void StartWatching(CommandOptions options)
         {
             var files = GetMatchingFiles(options);
+
+            var file = "";
+            var watcher = new FileSystemWatcher
+            {
+                Path = options.SourceDirectoryPath,
+                NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName,
+                Filter = file,
+            };
+
+            watcher.Changed += (sender, args) => Console.WriteLine("File has changed.");
+            watcher.Renamed += (sender, args) => Console.WriteLine("File has been renamed.");
+
+            // Start watching the file.
+            watcher.EnableRaisingEvents = true;
+
             Console.WriteLine(files.Select(a => a.Path).Aggregate((a, b) => a + ", " + b));
         }
+       
 
         private static IEnumerable<FilePatternMatch> GetMatchingFiles(CommandOptions options)
         {
