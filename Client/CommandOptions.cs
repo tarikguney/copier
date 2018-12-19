877f916ff7901227ee301d9b0b74179e0dc0fa63
Tarik Guney <atarikguney@gmail.com>
2018-12-18 18:45:46 -0700

    Added usage examples with verbose and override options

--- Client/CommandOptions.cs
+++ Client/CommandOptions.cs
@@ -6,30 +6,56 @@ namespace Copier.Client
 {
     public class CommandOptions
     {
-        [Option('s', "sourceDirectoryPath", HelpText="Parent directory where the files will be searched. If skipped, the current directory will be searched.", Required = false)]
+        [Option('s', "sourceDirectoryPath",
+            HelpText =
+                "Parent directory where the files will be searched. If skipped, the current directory will be searched.",
+            Required = false)]
         public string SourceDirectoryPath { get; set; }
-        
-        [Option('f',"fileGlobPattern", Required = true, HelpText = "Files to be searched. Accepts glob patter for pattern matching.")]
+
+        [Option('f', "fileGlobPattern", Required = true,
+            HelpText = "Files to be searched. Accepts glob patter for pattern matching.")]
         public string FileGlobPattern { get; set; }
-        
-        [Option('d', "destinationDirectoryPath",Required = true, HelpText = "Destination directory path")]
+
+        [Option('d', "destinationDirectoryPath", Required = true, HelpText = "Destination directory path")]
         public string DestinationDirectoryPath { get; set; }
-        
-        [Option('o',"overwriteTargetFiles", Default = false, Required = false, HelpText = "If passed true, copier will overwrite existing files at the target location.")]
+
+        [Option('o', "overwriteTargetFiles", Default = false, Required = false,
+            HelpText = "If passed true, copier will overwrite existing files at the target location.")]
         public bool OverwriteTargetFile { get; set; }
-        
-        [Option('v', "verbose", Default=false, Required = false, HelpText = "If passed true, more information will be outputted to the console.")]
-        public bool Verbose { get; set; }  
-        
+
+        [Option('v', "verbose", Default = false, Required = false,
+            HelpText = "If passed true, more information will be outputted to the console.")]
+        public bool Verbose { get; set; }
+
         [Usage]
-        public static IEnumerable<Example> Examples => new List<Example>() {
-            new Example("Starts the copier", new UnParserSettings() { PreferShortName = true },
+        public static IEnumerable<Example> Examples => new List<Example>()
+        {
+            new Example("Starts the copier", new UnParserSettings() {PreferShortName = true},
                 new CommandOptions
-            {
-                SourceDirectoryPath = "C:/Users/MyDocuments/Images",
-                FileGlobPattern = "*.jpg",
-                DestinationDirectoryPath = "C:/Users/MyDocuments/NewImages"
-            })
+                {
+                    SourceDirectoryPath = "C:/Users/MyDocuments/Images",
+                    FileGlobPattern = "*.jpg",
+                    DestinationDirectoryPath = "C:/Users/MyDocuments/NewImages"
+                }),
+            new Example("Starts the copier and overwrites the target files.",
+                new UnParserSettings() {PreferShortName = true},
+                new CommandOptions
+                {
+                    SourceDirectoryPath = "C:/Users/MyDocuments/Images",
+                    FileGlobPattern = "*.jpg",
+                    DestinationDirectoryPath = "C:/Users/MyDocuments/NewImages",
+                    OverwriteTargetFile = true
+                }),
+            new Example("Starts the copier and overwrites the target files and outputs verbose messages.",
+                new UnParserSettings() {PreferShortName = true},
+                new CommandOptions
+                {
+                    SourceDirectoryPath = "C:/Users/MyDocuments/Images",
+                    FileGlobPattern = "*.jpg",
+                    DestinationDirectoryPath = "C:/Users/MyDocuments/NewImages",
+                    OverwriteTargetFile = true,
+                    Verbose =  true
+                })
         };
     }
 }
