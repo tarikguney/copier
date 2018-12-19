5eea5d6d9c09c85c7457b539457e26104acbcaba
Tarik Guney <atarikguney@gmail.com>
2018-12-18 18:15:31 -0700

    Copying files and reacting based on override and verbose options

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,11 +10,8 @@ namespace Copier.Client
         {
             Parser.Default.ParseArguments<CommandOptions>(args)
                 .WithParsed(StartWatching)
-                .WithNotParsed(a =>
-                {
-                    Environment.Exit(1);
-                });
-            
+                .WithNotParsed(a => { Environment.Exit(1); });
+
             Console.WriteLine("Please press any key to exit.");
             Console.ReadLine();
         }
@@ -22,34 +19,59 @@ namespace Copier.Client
         private static void StartWatching(CommandOptions options)
         {
             Console.WriteLine("Watching has started...");
-            var sourceDirectoryPath = string.IsNullOrWhiteSpace(options.SourceDirectoryPath)
-                ? Directory.GetCurrentDirectory() 
+
+            options.SourceDirectoryPath = string.IsNullOrWhiteSpace(options.SourceDirectoryPath)
+                ? Directory.GetCurrentDirectory()
                 : options.SourceDirectoryPath;
-            
-            WatchFile(options.FileGlobPattern, sourceDirectoryPath);
+
+            WatchFile(options);
         }
 
-        private static void WatchFile(string filePattern, string sourceDirectoryPath)
+        private static void WatchFile(CommandOptions options)
         {
             var watcher = new FileSystemWatcher
             {
-                Path = sourceDirectoryPath,
+                Path = options.SourceDirectoryPath,
                 NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName,
-                Filter = filePattern,
+                Filter = options.FileGlobPattern,
             };
 
             watcher.Changed += (sender, args) =>
             {
-                if (args.ChangeType == WatcherChangeTypes.Changed)
+                if (args.ChangeType != WatcherChangeTypes.Changed) return;
+                
+                if (options.Verbose)
                 {
                     Console.WriteLine($"{args.Name} file has changed.");
                 }
-            };
-            
-            watcher.Renamed += (sender, args) => Console.WriteLine($"{args.OldName} has been renamed to {args.Name}.");
 
-            // Start watching the file.
+                CopyFile(options.SourceDirectoryPath, args.Name, options.DestinationDirectoryPath,
+                    options.OverwriteTargetFile);
+            };
+
+            watcher.Renamed += (sender, args) =>
+            {
+                if (options.Verbose)
+                {
+                    Console.WriteLine($"{args.OldName} has been renamed to {args.Name}.");
+                }
+
+                CopyFile(options.SourceDirectoryPath, args.Name, options.DestinationDirectoryPath,
+                    options.OverwriteTargetFile);
+            };
+
             watcher.EnableRaisingEvents = true;
+        }
+
+        private static void CopyFile(string sourceDirectoryPath, string fileName,
+            string targetDirectoryPath, bool overwriteTargetFile)
+        {
+            var absoluteSourceFilePath = Path.Combine(sourceDirectoryPath, fileName);
+            var absoluteTargetFilePath = Path.Combine(targetDirectoryPath, fileName);
+
+            if(File.Exists(absoluteTargetFilePath) && !overwriteTargetFile) return;
+            
+            File.Copy(absoluteSourceFilePath, absoluteTargetFilePath, overwriteTargetFile);
         }
     }
 }
